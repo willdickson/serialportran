88b1927b2fdbfaa2be6b88c3ac8f053006475bb7
Will Dickson <wbd@caltech.edu>
2019-12-12 11:54:11 -0800

    Worked on types.

--- src/serialport_types.F90
+++ src/serialport_types.F90
@@ -7,15 +7,7 @@ module serialport_types
 
 
     implicit none
-    private 
-
-    public buf_len
-    public SPU_OK
-    public SPU_ERR
-    public SP_TRANSPORT_NATIVE
-    public SP_TRANSPORT_USB
-    public SP_TRANSPORT_BLUETOOTH
-    public spu_port_info_t
+    public
 
     integer,  parameter ::  buf_len = SPU_BUF_LEN 
 
@@ -24,6 +16,93 @@ module serialport_types
         enumerator :: SPU_ERR = 1
     end enum
 
+    ! Port access modes
+    enum, bind(c) 
+        enumerator :: SP_MODE_READ       = 1
+        enumerator :: SP_MODE_WRITE      = 2
+        enumerator :: SP_MODE_READ_WRITE = 3
+    end enum
+
+    ! Port events
+    enum, bind(c) 
+        enumerator :: SP_EVENT_RX_READY = 1
+        enumerator :: SP_EVENT_TX_READY = 2
+        enumerator :: SP_EVENT_ERROR    = 4
+    end enum
+
+    ! Buffer selection
+    enum, bind(c) 
+        enumerator :: SP_BUF_INPUT  = 1
+        enumerator :: SP_BUF_OUTPUT = 2
+        enumerator :: SP_BUF_BOTH   = 3
+    end enum
+
+    ! Parity settings
+    enum, bind(c) 
+        enumerator :: SP_PARITY_INVALID = -1
+        enumerator :: SP_PARITY_NONE    =  0
+        enumerator :: SP_PARITY_ODD     =  1
+        enumerator :: SP_PARITY_EVEN    =  2
+        enumerator :: SP_PARITY_MARK    =  3
+        enumerator :: SP_PARITY_SPACE   =  4
+    end enum
+
+    ! RTS pin behaviour
+    enum, bind(c)
+        enumerator :: SP_RTS_INVALID      = -1
+        enumerator :: SP_RTS_OFF          =  0
+        enumerator :: SP_RTS_ON           =  1
+        enumerator :: SP_RTS_FLOW_CONTROL =  2
+    end enum
+
+    ! CTS pin behaviour
+    enum, bind(c)
+        enumerator :: SP_CTS_INVALID      = -1
+        enumerator :: SP_CTS_IGNORE       =  0
+        enumerator :: SP_CTS_FLOW_CONTROL =  1
+    end enum
+
+    ! DTR pin behaviour
+    enum, bind(c) 
+        enumerator :: SP_DTR_INVALID      = -1
+        enumerator :: SP_DTR_OFF          =  0
+        enumerator :: SP_DTR_ON           =  1
+        enumerator :: SP_DTR_FLOW_CONTROL =  2
+    end enum
+
+    ! DSR pin behaviour. 
+    enum, bind(c)
+        enumerator :: SP_DSR_INVALID      = -1
+        enumerator :: SP_DSR_IGNORE       =  0
+        enumerator :: SP_DSR_FLOW_CONTROL =  1
+    end enum
+    
+    ! XON/XOFF flow control behaviour. 
+    enum, bind(c)
+        enumerator :: SP_XONXOFF_INVALID  = -1
+        enumerator :: SP_XONXOFF_DISABLED =  0
+        enumerator :: SP_XONXOFF_IN       =  1
+        enumerator :: SP_XONXOFF_OUT      =  2
+        enumerator :: SP_XONXOFF_INOUT    =  3
+    end enum
+    
+    ! Standard flow control combinations. 
+    enum, bind(c)
+        enumerator :: SP_FLOWCONTROL_NONE    = 0
+        enumerator :: SP_FLOWCONTROL_XONXOFF = 1
+        enumerator :: SP_FLOWCONTROL_RTSCTS  = 2
+        enumerator :: SP_FLOWCONTROL_DTRDSR  = 3
+    end enum
+
+    ! Input signals
+    enum, bind(c) 
+        enumerator :: SP_SIG_CTS = 1
+        enumerator :: SP_SIG_DSR = 2
+        enumerator :: SP_SIG_DCD = 4
+        enumerator :: SP_SIG_RI  = 8
+    end enum
+
+    ! Transport types.
     enum, bind(c)
         enumerator :: SP_TRANSPORT_NATIVE    = 0
         enumerator :: SP_TRANSPORT_USB       = 1
